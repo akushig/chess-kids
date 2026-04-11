369d76e5fff08f710fe943591b2bddfec5d04674
akushig <akushig@gmail.com>
2026-04-11 16:24:56 +0900

    Auto-activate new service worker for instant cache updates
    Added skipWaiting() and clients.claim() so mobile browsers
    pick up new versions without manual cache clearing.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chess-kids-v3';
+const CACHE_NAME = 'chess-kids-v4';
 const ASSETS = [
   './',
   './index.html',
@@ -10,13 +10,14 @@ const ASSETS = [
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
